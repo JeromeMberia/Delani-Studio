a7586dec1a28d8cacd6abdf4e779388b7954c76f
JeromeMberia <jeromemberia@gmail.com>
2020-02-23 22:18:11 +0300

    add layout

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("p").click(function() {
+    $("#design1").toggle();
+    $("#design2").toggle();
+  });
+});
+$(document).ready(function(){
+  $("p").click(function() {
+    $("#development1").toggle();
+    $("#development2").toggle();
+  });
+});
+$(document).ready(function(){
+  $("p").click(function() {
+    $("#product1").toggle();
+    $("#product2").toggle();
+  });
+});
