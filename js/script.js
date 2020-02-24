4f9ca4a30b1954aa6d1c02baba5d47379fad2464
Jerome Mberia <60657652+JeromeMberia@users.noreply.github.com>
2020-02-24 16:24:17 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,6 +31,6 @@ function data() {
   }else if ( message === ''){
     alert("Please input the correct month")
   } else {
-      alert(name + " we have received your message. Thank you fro reaching out to us. ")
+      alert(name + " we have received your message. Thank you for reaching out to us. ")
   };
 };
